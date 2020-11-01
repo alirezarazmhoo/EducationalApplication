1e5a57a67b643a24f4ded8b3276f45d4844f7cdc
alirezarazmjoo <alireza1539razmjoo@yahoo.com>
2020-11-01 10:04:43 +0330

    FirstCommit

---
+++ EducationalApplication/Services/RepositoryBase.cs
@@ -0,0 +1,51 @@
+﻿using EducationalApplication.Data;
+using EducationalApplication.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace EducationalApplication.Services
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected ApplicationDbContext _DbContext;
+
+        public RepositoryBase(ApplicationDbContext DbContext)
+        {
+            this._DbContext = DbContext;
+        }
+        public IQueryable<T> FindAll(Expression<Func<T, object>> expression)
+        {
+            if (expression != null)
+            {
+                return this._DbContext.Set<T>().Include(expression).AsNoTracking();
+            }
+            else
+            {
+                return this._DbContext.Set<T>().AsNoTracking();
+
+            }
+
+        }
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this._DbContext.Set<T>()
+                .Where(expression).AsNoTracking();
+        }
+        public void Create(T entity)
+        {
+            this._DbContext.Set<T>().Add(entity);
+        }
+        public void Update(T entity)
+        {
+            this._DbContext.Set<T>().Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            this._DbContext.Set<T>().Remove(entity);
+        }
+    }
+}
---
+++ EducationalApplication/Services/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using EducationalApplication.Data;
+using EducationalApplication.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationalApplication.Services
+{
+	public class UnitOfWork : IUnitOfWorkRepo
+	{
+		private ApplicationDbContext _DbContext;
+		public UnitOfWork(ApplicationDbContext DbContext)
+		{
+			_DbContext = DbContext;
+		}
+		public async Task SaveAsync()
+		{
+			await _DbContext.SaveChangesAsync();
+		}
+	}
+}
